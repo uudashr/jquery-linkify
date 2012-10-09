c0f9d1ef0291cd6254b5af029dd49acd2ef69cd2
Dave Cowart <davecowart@gmail.com>
2012-10-08 21:44:50 -0500

    Added target option for links

--- jquery.linkify.js
+++ jquery.linkify.js
@@ -1,12 +1,12 @@
-function linkify(string, buildHashtagUrl, includeW3) {
+function linkify(string, buildHashtagUrl, includeW3, target) {
   if (includeW3) {
-    string = string.replace(/((http|https|ftp)\:\/\/|\bw{3}\.)[a-z0-9\-\.]+\.[a-z]{2,3}(:[a-z0-9]*)?\/?([a-z0-9\-\._\?\,\'\/\\\+&amp;%\$#\=~])*/gi, "<a href=\"$&\">$&</a>");
+    string = string.replace(/((http|https|ftp)\:\/\/|\bw{3}\.)[a-z0-9\-\.]+\.[a-z]{2,3}(:[a-z0-9]*)?\/?([a-z0-9\-\._\?\,\'\/\\\+&amp;%\$#\=~])*/gi, "<a href=\"$&\" target=\"" + target + "\">$&</a>");
   } else {
-    string = string.replace(/(http|https|ftp)\:\/\/[a-z0-9\-\.]+\.[a-z]{2,3}(:[a-z0-9]*)?\/?([a-z0-9\-\._\?\,\'\/\\\+&amp;%\$#\=~])*/gi, "<a href=\"$&\">$&</a>");
+    string = string.replace(/(http|https|ftp)\:\/\/[a-z0-9\-\.]+\.[a-z]{2,3}(:[a-z0-9]*)?\/?([a-z0-9\-\._\?\,\'\/\\\+&amp;%\$#\=~])*/gi, "<a href=\"$&\" target=\"" + target + "\">$&</a>");
   }
   
   if (buildHashtagUrl) {
-    string = string.replace(/\B#(\w+)/g, "<a href=" + buildHashtagUrl("$1") +">#$1</a>");
+    string = string.replace(/\B#(\w+)/g, "<a href=" + buildHashtagUrl("$1") +" target=\"" + target + "\">#$1</a>");
   }
   return string;
 }
@@ -17,6 +17,7 @@ function linkify(string, buildHashtagUrl, includeW3) {
       var $this = $(this);
       var buildHashtagUrl;
       var includeW3 = true;
+      var target = '_self';
       if (opts) {
         if (typeof opts  == "function") {
           buildHashtagUrl = opts;
@@ -27,9 +28,12 @@ function linkify(string, buildHashtagUrl, includeW3) {
           if (typeof opts.includeW3 == "boolean") {
             includeW3 = opts.includeW3;
           }
+          if (typeof opts.target == "string") {
+            target = opts.target;
+          }
         }
       }
-      $this.html(linkify($this.html(), buildHashtagUrl, includeW3));
+      $this.html(linkify($this.html(), buildHashtagUrl, includeW3, target));
     });
   }
 })(jQuery);
