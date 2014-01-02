9053e5a7184e3532c65908c3337c64066934ec14
Nuruddin Ashr <uudashr@gmail.com>
2014-01-02 07:12:55 +0700

    add 'noFollow' options

--- jquery.linkify.js
+++ jquery.linkify.js
@@ -1,4 +1,9 @@
-function linkify(string, buildHashtagUrl, includeW3, target) {
+function linkify(string, buildHashtagUrl, includeW3, target, noFollow) {
+  relNoFollow = "";
+  if (noFollow) {
+    relNoFollow = " rel=\"nofollow\"";
+  }
+  
   string = string.replace(/((http|https|ftp)\:\/\/|\bw{3}\.)[a-z0-9\-\.]+\.[a-z]{2,3}(:[a-z0-9]*)?\/?([a-z\u00C0-\u017F0-9\-\._\?\,\'\/\\\+&amp;%\$#\=~])*/gi, function(captured) {
     var uri;
     if (captured.toLowerCase().indexOf("www.") == 0) {
@@ -9,11 +14,11 @@ function linkify(string, buildHashtagUrl, includeW3, target) {
     } else {
       uri = captured;
     }
-    return "<a href=\"" + uri+ "\" target=\"" + target + "\" rel=\"nofollow\">" + captured + "</a>";;
+    return "<a href=\"" + uri+ "\" target=\"" + target + "\"" + relNoFollow + ">" + captured + "</a>";
   });
   
   if (buildHashtagUrl) {
-    string = string.replace(/\B#(\w+)/g, "<a href=" + buildHashtagUrl("$1") +" target=\"" + target + "\" rel=\"nofollow\">#$1</a>");
+    string = string.replace(/\B#(\w+)/g, "<a href=" + buildHashtagUrl("$1") +" target=\"" + target + "\"" + relNoFollow + ">#$1</a>");
   }
   return string;
 }
@@ -25,6 +30,7 @@ function linkify(string, buildHashtagUrl, includeW3, target) {
       var buildHashtagUrl;
       var includeW3 = true;
       var target = '_self';
+      var noFollow = true;
       if (opts) {
         if (typeof opts  == "function") {
           buildHashtagUrl = opts;
@@ -38,6 +44,9 @@ function linkify(string, buildHashtagUrl, includeW3, target) {
           if (typeof opts.target == "string") {
             target = opts.target;
           }
+          if (typeof opts.noFollow == "boolean") {
+            noFollow = opts.noFollow;
+          }
         }
       }
       $this.html(
@@ -45,7 +54,7 @@ function linkify(string, buildHashtagUrl, includeW3, target) {
             $this.contents(),
             function(n, i) {
                 if (n.nodeType == 3) {
-                    return linkify(n.data, buildHashtagUrl, includeW3, target);
+                    return linkify(n.data, buildHashtagUrl, includeW3, target, noFollow);
                 } else {
                     return n.outerHTML;
                 }
